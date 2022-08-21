1fbfb084754a9bdbf944505f92fe71ff6c4f597a
uiz-11 <ui.zhang11@gmail.com>
2022-08-21 15:40:09 -0500

    WEAT-5 Add /lucky endpoint

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 from flask import Flask
 
+import pandas as pd
+import random
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return {'message': 'hi'}
+
+
+@app.route('/lucky')
+def lucky():
+    names = pd.read_csv('restaurants.csv')['name'].tolist()
+    picked_name = random.choice(names)
+
+    return picked_name
