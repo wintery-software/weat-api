e195a00fc3c4bacc9137c360b93df661715de4d0
uiz-11 <ui.zhang11@gmail.com>
2022-08-21 21:17:06 -0500

    WEAT-12 Populate restaurant list in database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'csv'
+
+Restaurant.delete_all
+restaurants = CSV.read('data/restaurants.csv', headers: true).map(&:to_h)
+Restaurant.create(restaurants)
