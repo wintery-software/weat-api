6cebffa3846b78d92db9fef45de50776d63952d8
uiz-11 <ui.zhang11@gmail.com>
2022-08-21 21:40:34 -0500

    WEAT-5 Remove as from route

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get '/lucky', to: 'application#lucky', as: 'lucky'
+  get '/lucky', to: 'application#lucky'
 end
