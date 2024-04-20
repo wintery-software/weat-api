e9a46dfd4533cb2775bbf41588da8c35150faffc
uiz-11 <ui.zhang11@gmail.com>
2024-04-20 15:56:28 -0400

    feat: add /apis endpoint

--- app/routes/health_routes.py
+++ app/routes/health_routes.py
@@ -3,3 +3,14 @@ from http import HTTPStatus
 
 def health():
     return "OK", HTTPStatus.OK
+
+
+def list_apis():
+    apis = [
+        "/trending/restaurants",
+        "/trending/restaurant-items",
+        "/restaurants",
+        "/restaurants/categories",
+    ]
+
+    return apis, HTTPStatus.OK
