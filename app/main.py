47b6903bc73c110271eb820b42dd6917087443bc
uiz-11 <ui.zhang11@gmail.com>
2025-04-18 15:24:57 -0700

    feat: add cors middleware (#41)

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
+import os
 from fastapi import FastAPI, HTTPException, Request
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routes.places import router as places_router
 from app.routes.tags import router as tags_router
@@ -19,3 +21,14 @@ async def handle_custom_error(request: Request, exc: Exception):
         status_code=HTTPStatus.BAD_REQUEST,
         detail=str(exc),
     )
+
+
+origins = os.getenv("CORS_ALLOW_ORIGINS", "").split(",")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["Authorization", "Content-Type"],
+)
